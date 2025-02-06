84db84b5d967b14172bbe0fb064ff4e6bad3e383
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-02-06 15:32:03 -0300

    Adicionando exercicio27

---
+++ Exercicios_slides/exercicio27.py
@@ -0,0 +1,11 @@
+#Dado um número inteiro, seu valor invertido é ele lido de trás
+#para frente
+
+def invertido(x):
+    invertido = 0
+    while x > 0:
+        invertido = invertido*10 + x%10
+        x = x//10
+    return invertido
+
+print(invertido(12345678))
