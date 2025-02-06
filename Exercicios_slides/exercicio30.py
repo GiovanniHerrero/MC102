9b1e7f1ea214b864c4c152978b5ee35b17c039f2
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-02-06 19:14:19 -0300

    Adicionando exercicio30

---
+++ Exercicios_slides/exercicio30.py
@@ -0,0 +1,16 @@
+#Faça uma função que, dado um número n e um valor v (por
+#padrão, valendo 0), cria uma lista de n posições com cada posição
+#valendo v. Documente sua função.
+
+def cria_lista_n_valores_v(n,v):
+    """Cria uma lista de n posições, cada valendo uma valendo v
+
+    Se n é negativo ou nulo, devolve lista vazia
+
+    Parâmetros:
+
+    n -- número de elementos na lista
+    v -- valor de cada elemento na lista
+    """
+    return [v]*n
+
