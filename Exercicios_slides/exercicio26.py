ddcae40e2343ca148eabe1b3c40d9c02dc1f998c
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-02-06 14:37:16 -0300

    Adicionando exercicio26

--- Exercicios_slides/exercicio26.py
+++ Exercicios_slides/exercicio26.py
@@ -3,3 +3,17 @@
 
 #Faça uma função que, dados dois números m e n, decide se são
 #amigáveis.
+
+def soma_divisores_proprios(x):
+    soma = 0
+    for i in range(1,(x//2) + 1):
+        if x%i == 0:
+            soma +=i
+    return soma
+
+def sao_amigaveis(x,y):
+    s_x = soma_divisores_proprios(x)
+    s_y = soma_divisores_proprios(y)
+    
+    return (y == s_x or x == s_y)
+
