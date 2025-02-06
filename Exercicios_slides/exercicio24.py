e44efc86730675b109e26a2f2004c626a501f3ea
Giovanni Barbosa Herrero <giovanni.herrero22@gmail.com>
2025-02-06 14:01:54 -0300

    Adicionando exercicio24

--- Exercicios_slides/exercicio24.py
+++ Exercicios_slides/exercicio24.py
@@ -1,5 +1,9 @@
 #Faça uma função que recebe uma lista e devolve uma nova
 #lista invertida.
 
-def inverte_lista():
-    for i in 
+def inverte_lista(lista):
+    for i in range(len(lista)//2):
+        lista[i],lista[len(lista) - 1 -i] = lista[len(lista) -1 -i], lista[i]
+    return lista
+
+print(inverte_lista([1,2,3,4,5,6,7,8,9,10,11]))
